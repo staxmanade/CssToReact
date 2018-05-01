0f2d09b4064b21a3eb276e3a6800a38aab4c69f8
htbkoo <h1017760@connect.hku.hk>
2018-05-01 18:56:19 +0800

    made bin/rename.js more generic; updated script in package.json accordingly htbkoo/CssToAndFromReact

--- bin/rename.js
+++ bin/rename.js
@@ -1,6 +1,17 @@
-let mv = require('mv');
+const args = require('args');
+const mv = require('mv');
 
-let source = 'build', dest = 'docs';
+args
+    .option('source', 'The source to rename from')
+    .option('dest', 'The destination to rename to')
+    .example('node ./bin/rename -s build -d docs', 'To rename the file/folder "build" to "docs"');
+
+let {source, dest} = args.parse(process.argv);
+
+if (!source || !dest){
+    args.showHelp();
+}
+
 mv(source, dest, function (err) {
     // done. it tried fs.rename first, and then falls back to
     // piping the source file to the dest file and then unlinking
